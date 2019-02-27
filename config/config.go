7a53e4595b66208a24b9588c60391b84a105d630
yongchang <50730756@qq.com>
2019-02-27 10:05:28 +0800

    update config

--- config/config.go
+++ config/config.go
@@ -1,20 +1,5 @@
 package config
 
-// API KEY
-//var (
-//	// todo: replace with your own AccessKey and Secret Key
-//	ACCESS_KEY string = "00171493-e390f9e7-378efc78-eb742"
-//	SECRET_KEY string = "51ea6758-c16a9c80-d9b75ea9-c6426"
-//
-//	// default to be disabled, please DON'T enable it unless it's officially announced.
-//	ENABLE_PRIVATE_SIGNATURE bool = false
-//
-//	// generated the key by: openssl ecparam -name prime256v1 -genkey -noout -out privatekey.pem
-//	// only required when Private Signature is enabled
-//	// todo: replace with your own PrivateKey from privatekey.pem
-//	PRIVATE_KEY_PRIME_256 string = `xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`
-//)
-
 // API请求地址, 不要带最后的/
 var (
 	//todo: replace with real URLs and HostName
